2b3e12d6cb012da07538fd596ab94718cb0d3d0a
senannetwork <67416841+senannetwork@users.noreply.github.com>
2021-12-27 19:06:54 +0100

    27122021

---
+++ W3/connect.php
@@ -0,0 +1,8 @@
+<?php
+    $dbserver = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $dbname = 'w1';
+    $con = mysqli_connect($dbserver, $user, $pass, $dbname);
+    // if($con){echo 'connection done';}
+    // else {echo 'Please check the connection';}
